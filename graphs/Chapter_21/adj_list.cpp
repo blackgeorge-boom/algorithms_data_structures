7dd88e5962bc18867b57d21d37f3b91fa3596f39
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2019-03-07 13:41:10 +0200

    Added 22.1-3

--- graphs/Chapter_21/adj_list.cpp
+++ graphs/Chapter_21/adj_list.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <vector>
+#include <boost/range/combine.hpp>
+#include <boost/tuple/tuple.hpp>
 
 class Vertex {
     char n;
@@ -14,7 +16,7 @@ public:
     ~Vertex() = default;
 
     char name() const { return n; }
-    std::vector<std::pair<Vertex, int>> const adjacents() const { return adj; }
+    std::vector<std::pair<Vertex, int>> adjacents() { return adj; }
 
     void add_edge(const Vertex& v, int w);
     void add_edge_u(Vertex& v, int w);
@@ -64,13 +66,20 @@ void Graph::print_vertices() const {
     for (auto& i : vv) i.print_adj();
 }
 
-Graph reverse_graph(Graph& g) {
+// TODO:
+void reverse_graph(Graph& g) {
     Graph rev_g;
     for (auto& u : g.vertices())
         rev_g.add_vertex(Vertex(u.name()));
-    for (auto& u : g.vertices()) {
-        for (auto& v : u.adjacents()) {
+}
 
+void mult_to_undir(Graph& g) {
+    auto vert_num = g.vertices().size();
+    int A[vert_num];
+    for (auto& u : g.vertices()) {
+        for (auto i = 0; i < u.adjacents().size(); i++) {
+            if (u.name() == u.adjacents()[i].first.name())
+                u.adjacents().erase(u.adjacents().begin() + i);
         }
     }
 }
